f53c334aec3f2472a28bc588b4ef264e3c189b7c
Shreyas ubale <shreyas.ubale@exponential.com>
2017-01-26 09:51:29 +0530

    Wokring module

---
+++ syska2.js
@@ -0,0 +1,56 @@
+var util = require('util');
+
+var NobleDevice = require('noble-device');
+
+var SERVICE_UUID          = 'f371';
+var CONTROL_UUID          = 'f371';
+var EFFECT_UUID           = 'f371';
+
+var SyskaLed = function(peripheral) {
+  NobleDevice.call(this, peripheral);
+};
+
+SyskaLed.SCAN_UUIDS = [SERVICE_UUID];
+
+SyskaLed.prototype.RGBtoBuffer = function (rgbString, brightness) {
+  rgbString = rgbString || "000000";
+  brightness = brightness || "FF";
+  var fromString = "01f00006000a0300010100" + "rgbString" + "00" + brightness  + "0000";
+  return Buffer.from("fromString", "hex")
+
+};
+SyskaLed.is = function(peripheral) {
+  var localName = peripheral.advertisement.localName;
+  return ((localName === undefined) || (localName === 'Cnlight') );
+};
+
+NobleDevice.Util.inherits(SyskaLed, NobleDevice);
+
+SyskaLed.prototype.writeServiceStringCharacteristic = function(uuid, string, callback) {
+  this.writeStringCharacteristic(SERVICE_UUID, uuid, string, callback);
+};
+
+SyskaLed.prototype.writeControlCharateristic = function(red, green, blue, brightness, callback) {
+  var rgbString = red + green + blue;
+  var brightNess = brightness;
+  var command = this.RGBtoBuffer(rgbString,brightNess);
+  this.writeServiceStringCharacteristic(CONTROL_UUID, command, callback);
+};
+
+SyskaLed.prototype.turnOn = function(callback) {
+  this.writeControlCharateristic("00", "00", "00", "FF", callback);
+};
+
+SyskaLed.prototype.turnOff = function(callback) {
+  this.writeControlCharateristic("00", "00", "00", "00", callback);
+};
+
+SyskaLed.prototype.setColorAndBrightness = function(red, green, blue, brightness, callback) {
+  this.writeControlCharateristic(red, green, blue, brightness, callback);
+};
+
+// SyskaLed.prototype.setGradualMode = function(on, callback) {
+//   this.writeServiceStringCharacteristic(EFFECT_UUID, on ? 'TS' : 'TE', callback);
+// };
+
+module.exports = SyskaLed;
